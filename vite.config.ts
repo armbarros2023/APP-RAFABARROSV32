aaed1b3930ea584434487a6dc294a9f24a680a58
Armando de Barros <130599357+armbarros2023@users.noreply.github.com>
2026-05-11 12:30:33 -0300

    Fix local API proxy target

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
         proxy: {
           '/api': {
-            target: env.VITE_API_PROXY_TARGET || 'http://localhost:5001',
+            target: env.VITE_API_PROXY_TARGET || 'http://localhost:5000',
             changeOrigin: true,
           },
         },
